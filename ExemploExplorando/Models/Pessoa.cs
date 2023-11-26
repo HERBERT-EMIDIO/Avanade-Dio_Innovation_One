f23fafa8453a26b1dfd88903b933c910822bbd79
Herbert Emidio <herbertemidio2017@gmail.com>
2023-11-26 18:36:04 -0300

    proteção do get e set

--- ExemploExplorando/Models/Pessoa.cs
+++ ExemploExplorando/Models/Pessoa.cs
@@ -9,10 +9,14 @@ namespace ExemploExplorando.Models
     {
         private string _nome;
 
-
+        private int _idade;
 
         public string Nome
         {
+
+            //uma forma de escrever com ayron =>
+            //get => _nome.ToUpper();
+
             get
             {
                 return _nome.ToUpper();
@@ -22,14 +26,27 @@ namespace ExemploExplorando.Models
             {
                 if (value == "")
                 {
-                    throw new ArgumentException("O nome não pode ser vazio");
+                    throw new ArgumentException("O nome não pode ser vazio!");
                 }
                 _nome = value;
             }
         }
 
 
-        public int Idade { get; set; }
+        public int Idade
+        {
+            get => _idade;
+            
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Idade inválida!");
+                }
+
+                _idade = value;
+            }
+        }
 
 
 
